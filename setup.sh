1b2c883bb08c130de8cfb366f153f9b06cd6bbd7
Benjamin Packer <benjaminpacker16@gmail.com>
2020-03-11 02:39:50 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ pip3 install matplotlib numpy pandas scikit-learn scipy sklearn tabulate
 wget "https://raw.githubusercontent.com/bpacke/903/master/generated_datasets.py"
 wget "https://raw.githubusercontent.com/bpacke/903/master/algorithm_test.py"
 mkdir test_output
-nano algorithm_test.py &
 clear
 echo "Don't forget to pip3 install any other packages you may need"
+echo "You need to wget your code"
+echo "Use nano algorithm_test.py to import your algorithm"
 echo "Run the test with python3 algorithm_test.py"
