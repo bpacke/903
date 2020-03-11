b7e2c9f6335a7639ef333c13f373c6fede149dc1
Benjamin Packer <benjaminpacker16@gmail.com>
2020-03-11 20:55:40 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ apt-get install python3-pip
 pip3 install matplotlib numpy pandas scikit-learn scipy sklearn tabulate
 wget "https://raw.githubusercontent.com/bpacke/903/master/generated_datasets.py"
 wget "https://raw.githubusercontent.com/bpacke/903/master/algorithm_test.py"
-mkdir test_output
 clear
 echo "Don't forget to pip3 install any other packages you may need"
 echo "You need to wget your code"
