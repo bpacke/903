db676a29e4b064eac739faefc86b153575366adf
Benjamin Packer <benjaminpacker16@gmail.com>
2020-03-11 02:32:24 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ pip3 install matplotlib numpy pandas scikit-learn scipy sklearn tabulate
 wget "https://raw.githubusercontent.com/bpacke/903/master/generated_datasets.py"
 wget "https://raw.githubusercontent.com/bpacke/903/master/algorithm_test.py"
 mkdir test_output
+nano algorithm_test.py
 clear
 echo "Don't forget to pip3 install any other packages you may need"
+echo "Run the test with python3 algorithm_test.py"
